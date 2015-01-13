35fc311274538f244a45823f96c292a21ecda8ba
Tanay PrabhuDesai <tanayseven@gmail.com>
2015-01-13 19:54:30 +0530

    Initial commit

---
+++ index_files/main_code.js
@@ -0,0 +1,56 @@
+    $(document).ready(function(){
+      jQuery.fn.center = function () {
+        this.css("position","absolute");
+        this.css("top", Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + $(window).scrollTop()) + "px");
+        this.css("left", Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + $(window).scrollLeft()) + "px");
+        return this;
+      }
+      $(".info").hide();
+      $(".accordian").center();
+      $(".info").center();
+      $('.accordian').click(function(e){
+        e.preventDefault();
+        $('.accordian').slideUp();
+      });
+      $('.back').click(function(e){
+        e.preventDefault();
+        $(".info").fadeOut();
+        $('.accordian').slideDown();
+      });
+      $(".hackathon").click(function(e){
+          e.preventDefault();
+          $(".info").hide();
+          $("#div_hackathon").fadeIn();
+          console.log("hackathon");
+      });
+      $(".line_follower").click(function(e){
+          e.preventDefault();
+          $(".info").hide();
+          $("#div_line_follower").fadeIn();
+          console.log("line_follower");
+      });
+      $(".inquizitive").click(function(e){
+          e.preventDefault();
+          $(".info").hide();
+          $("#div_inquizitive").fadeIn();
+          console.log("inquizitive");
+      });
+      $(".robo_kombat").click(function(e){
+          e.preventDefault();
+          $(".info").hide();
+          $("#div_robo_kombat").fadeIn();
+          console.log("robo_kombat");
+      });
+      $(".technical_innovations").click(function(e){
+          e.preventDefault();
+          $(".info").hide();
+          $("#div_technical_innovations").fadeIn();
+          console.log("technical_innovations");
+      });
+      $(".catapult").click(function(e){
+          e.preventDefault();
+          $(".info").hide();
+          $("#div_catapult").fadeIn();
+          console.log("catalpult");
+      });
+    });
